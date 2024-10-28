c9d3d1ca2d3447fef7c596a1732b1cad64f187dd
Cici <119031599+Cici919@users.noreply.github.com>
2024-10-28 18:35:28 -0500

    Update logger.h

--- src/common/logger.h
+++ src/common/logger.h
@@ -2,7 +2,7 @@
 #define LOGGER_H
 
 void init_logger(const char *filename);
-void log_message(const char *message);
+void log_info(const char *format, ...); 
 void close_logger();
 
 #endif // LOGGER_H
